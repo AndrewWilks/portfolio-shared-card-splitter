8662dc4e7cc352e79a2e0556e5698c81ea88a293
Wilksy <a.wilksy92@gmail.com>
2025-11-13 08:37:56 +1000

    feat: wrap RootComponent with Providers and RootLayout for context management

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import RootLayout from "../components/layout/index.tsx";
+import Providers from "../context/index.tsx";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -7,8 +8,10 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <React.Fragment>
-      <Outlet />
-    </React.Fragment>
+    <Providers>
+      <RootLayout>
+        <Outlet />
+      </RootLayout>
+    </Providers>
   );
 }
