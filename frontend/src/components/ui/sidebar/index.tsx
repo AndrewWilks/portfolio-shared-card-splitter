9e4691a3616db13d818439d643d9d203c1237b1f
Wilksy <a.wilksy92@gmail.com>
2025-11-18 18:57:03 +1000

    feat: add sidebar components and context for sidebar functionality

---
+++ frontend/src/components/ui/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export * as _config from "./.config.ts";
+
+export * from "./sidebarProvider.tsx";
+export * from "./sidebar.tsx";
+
+export * from "./hook/index.tsx";
+export * from "./components/index.tsx";
