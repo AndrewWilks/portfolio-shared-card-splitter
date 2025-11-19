01536aaa4e210572c36199ad3399524cd177dd96
Wilksy <a.wilksy92@gmail.com>
2025-11-19 16:46:06 +1000

    refactor: update user and card types for consistency and clarity

--- backend/db/types.ts
+++ backend/db/types.ts
@@ -2,9 +2,12 @@ import * as schema from "./schema.ts";
 import { InferSelectModel, InferInsertModel, InferEnum } from "drizzle-orm";
 
 export type DB_User = InferSelectModel<typeof schema.usersTable>;
-export type DB_NewUser = InferInsertModel<typeof schema.usersTable>;
+export type DB_UserKey = DB_User["id"];
+export type DB_UserCreate = InferInsertModel<typeof schema.usersTable>;
+export type DB_UserUpdate = Partial<Omit<DB_User, "id">>;
 
 export type DB_Card = InferSelectModel<typeof schema.cardsTable>;
-export type DB_NewCard = InferInsertModel<typeof schema.cardsTable>;
-
+export type DB_CardKey = DB_Card["id"];
 export type DB_CardType = InferEnum<typeof schema.cardTypeEnum>;
+export type DB_CardCreate = InferInsertModel<typeof schema.cardsTable>;
+export type DB_CardUpdate = Partial<Omit<DB_Card, "id">>;
